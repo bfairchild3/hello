67ce24a9e505d01b67cd4c5124298792d4b894d7
BRENT_FAIRCHILD <bfairchild3@huskers.unl.edu>
2020-07-12 03:49:16 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author Brent Fairchild
+ * Date 2020
+ * 
+ * A simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("Brent Fairchild!\n");
+     printf("Electrical Engineering");
+     
+     return 0; 
+     
+ }
