9c8a45a59e3017665edde8a7c7e4ae9222517fbd
BRENT_FAIRCHILD <bfairchild3@huskers.unl.edu>
2020-08-20 16:36:03 +0000

    capitalization error

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 /**
  * Author Brent Fairchild bfairchild3@huskers.unl.edu
- * Spencer SChmidt sschmidt21@huskers.unl.edu
+ * Spencer Schmidt sschmidt21@huskers.unl.edu
  * Date 2020/08/20
  *
  * A simple hello world program
